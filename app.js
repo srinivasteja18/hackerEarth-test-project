79228b19470a30fead8053ec4fbcc446d01975b7
Srinivas Teja <srinivasmoparthi17@gmail.com>
2021-12-06 15:36:04 +0530

    update

--- app.js
+++ app.js
@@ -44,7 +44,14 @@ app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", stripeRoutes);
 
-app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // app.get("*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
